5ced1b635fef182e74b49892976d91f1aaa5ee55
Firdaus <mohdfirdaus94@outlook.com>
2021-12-06 16:54:23 +0800

    Add day 4 class code

---
+++ day4/src/main/java/ibf2021/d4/ServerTestClient.java
@@ -0,0 +1,41 @@
+package ibf2021.d4;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerTestClient {
+
+    public static void main( String[] args ) throws IOException{
+        Socket socket = new Socket ("localhost", 3000 );
+
+
+    try (OutputStream os= socket.getOutputStream()) {
+        BufferedOutputStream bos= new BufferedOutputStream(os);
+        DataOutputStream dos= new DataOutputStream(bos);
+        Scanner myScan =new Scanner(System.in);
+        BufferedReader buff =new BufferedReader(new InputStreamReader(System.in));
+
+        String string="";
+        while(!string.equals("exit")){
+            string=buff.readLine();
+            dos.writeUTF(string);
+            dos.flush();
+        }
+
+    }
+    finally{
+        
+        socket.close();
+    }
+    
+    }
+}
+
+
---
+++ day4/src/main/java/ibf2021/d4/ServerTestServer.java
@@ -0,0 +1,37 @@
+package ibf2021.d4;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerTestServer {
+    public static void main( String[] args ) throws IOException{
+
+    ServerSocket server =new ServerSocket(3000);
+     System.out.println("Listening at port 3000...");
+    Socket socket=server.accept();
+
+    try (InputStream is = socket.getInputStream()) {
+        DataInputStream dis = new DataInputStream(new BufferedInputStream(is));
+        String string =dis.readUTF();
+
+        while(!string.equals("exit")){
+            System.out.println("Message: "+string);
+            string =dis.readUTF();
+        }
+        }
+    
+    finally{
+        socket.close();
+        server.close();
+    }
+        
+    
+    }
+}
+
+
